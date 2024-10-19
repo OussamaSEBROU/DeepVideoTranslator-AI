b4a076e1173086d684aa8974bbf00185e0560136
Mr. Oussama Sebrou <90466380+OussamaSEBROU@users.noreply.github.com>
2024-10-19 22:57:54 +0200

    Update demo.py

--- demo.py
+++ demo.py
@@ -51,7 +51,7 @@ def check_video_duration(video_file):
     video.close()
     os.unlink(tmp_file_path)
 
-    return duration <= 600  # 600 seconds = 10 minutes
+    return duration <= 800  # 600 seconds = 10 minutes
 
 def transcribe_video(video_file):
     """
